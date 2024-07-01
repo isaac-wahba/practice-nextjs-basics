d1dea54b8271f8db764ef9dc1ebd8b3643dacd10
isaac-wahba <isaac.wahba2511@gmail.com>
2024-07-01 07:21:43 +0300

    practice metadata and dynamic metadata , and dynamic metadata with async calls

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,21 @@
-export default function ProductDetails({
-  params,
-}: {
+import { Metadata } from "next";
+type Props = {
   params: {
     productId: string;
   };
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) =>
+    setTimeout(() => resolve(`Product ${params.productId}`), 100)
+  );
+  return {
+    title: title as string,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return <h1>Details about Product {params.productId}</h1>;
 }
