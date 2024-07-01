6b6c820b468f52c8e888cb9257f2062de6dff087
isaac-wahba <isaac.wahba2511@gmail.com>
2024-07-01 07:28:40 +0300

    practice metadata - title and dynamic titles with absolute, template, and default

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,3 @@
-import React from "react";
-export const metadata = {
-  title: "About ",
-};
 export default function About() {
   return <div>About me</div>;
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Blog",
+};
 export default function Blog() {
   return <div>Blog</div>;
 }
